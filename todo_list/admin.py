bf61ee0ba2ff5d440a23e643d7d823a05f235f34
Sergey <rinoto_kun@gmail.com>
2026-08-08 15:22:50 +0300

    Register Task and Tag in admin site

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from todo_list.models import Task, Tag
+
+
+admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.unregister(Group)
